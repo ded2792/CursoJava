857ead6b1289298216a4414e46fb80d0379b8608
ded2792 <ded27@live.com.ar>
2025-07-11 19:41:39 -0300

    Nuevas lecciones

---
+++ Java/05_Operadores/Operadores/src/OperadoresAritmeticos.java
@@ -0,0 +1,21 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println("*** Operadores Aritmeticos ***");
+        int a = 5, b = 3, resultado;
+        // Suma +
+        resultado = a + b;
+        System.out.println("Resultado suma = " + resultado);
+        // Resta -
+        resultado = a - b;
+        System.out.println("Resultado resta = " + resultado);
+        // Multiplicacion
+        resultado = a * b;
+        System.out.println("Resultado multiplicacion = " + resultado);
+        // Division
+        resultado = a / b;
+        System.out.println("Resultado division = " + resultado);
+        // Modulo
+        resultado = a % b;
+        System.out.println("Resultado del modulo: " + resultado);
+    }
+}
