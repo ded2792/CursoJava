50597b20ac040d080186ea5e941e562942372610
ded2792 <ded27@live.com.ar>
2025-07-14 23:28:38 -0300

    operadores not

---
+++ Java/05_Operadores/Operadores/src/OperadorNot.java
@@ -0,0 +1,9 @@
+public class OperadorNot {
+    public static void main(String[] args) {
+        System.out.println("*** Operedor not ***");
+        boolean a = false;
+        // operador not ! (invertir el valor logico)
+        var resultado = !a;
+        System.out.println("resultado = " + resultado);
+    }
+}
