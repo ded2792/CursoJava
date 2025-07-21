f7c35a8e273d6c719adf6812e1813adb3c74ce17
ded2792 <ded27@live.com.ar>
2025-07-21 00:05:57 -0300

    continuacion

---
+++ Java/06_SentenciasDeDecision/untitled/src/SentenciaIf.java
@@ -0,0 +1,10 @@
+public class SentenciaIf {
+    public static void main(String[] args) {
+        // Use de la sentencia if
+        var edad = 30;
+
+        if(edad >= 18){
+            System.out.println("Eres mayor de edad");
+        }
+    }
+}
