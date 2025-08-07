4ae6e172e09aa58d5f9fb146e2907754084ebda3
ded2792 <ded27@live.com.ar>
2025-08-07 17:22:57 -0300

    CreacionValidacionPassword

--- Java/07_Ciclos/src/CreacionValidacionPassword.java
+++ Java/07_Ciclos/src/CreacionValidacionPassword.java
@@ -17,3 +17,26 @@ public class CreacionValidacionPassword {
         System.out.println("Password válido.");
     }
 }
+// Solución del curso
+//
+//import java.util.Scanner;
+//
+//public class ValidacionPassword {
+//    public static void main(String[] args) {
+//        var consola = new Scanner(System.in);
+//
+//        System.out.println("*** Validación de Password ***");
+//
+//        System.out.print("Ingresa un password (de al menos 6 caracteres): ");
+//        var password = consola.nextLine();
+//
+//        // Valida el password
+//        while (password.length() < 6){
+//            System.out.println("El password no cumple con los requisitos. " +
+//                    "Debe tener al menos 6 caracteres");
+//            System.out.print("Ingresa un nuevo valor de password: ");
+//            password = consola.nextLine();
+//        }
+//        System.out.println("El valor de password es válido: " + password);
+//    }
+//}
