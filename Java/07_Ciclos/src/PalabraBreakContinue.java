9510bfbef5084a73a6fe06f9af12a5a89beb1f4e
ded2792 <ded27@live.com.ar>
2025-08-10 22:49:00 -0300

    Break y Continue

--- Java/07_Ciclos/src/PalabraBreakContinue.java
+++ Java/07_Ciclos/src/PalabraBreakContinue.java
@@ -3,11 +3,20 @@ public class PalabraBreakContinue {
         System.out.println("*** Break y Continue ***");
 
         // Ejemplo breack, imprimir solo el primer numero par
-        for (var numero = 1; numero < 10; numero++){
-            if (numero % 2 == 0){
+        for (var numero = 1; numero < 10; numero++) {
+            if (numero % 2 == 0) {
                 System.out.print(numero + " ");
                 break;
             }
+        }
+
+        // Ejemplo continue, imprimir solo numeros pares, ignorar impares
+        System.out.println("\nPalabra continue: ");
+        for (var numero = 1; numero < 10; numero++) {
+            if (numero % 2 == 1) { // numero impar
+                continue;// saltamos a la siguiente iterecion
+            }
+            System.out.print(numero + " "); // numeros pares
         }
     }
 }
